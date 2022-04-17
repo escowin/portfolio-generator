108391db5bcd838cdfd43066c30e89a3e6ec061b
escowin <edwin@escowinart.com>
2022-04-16 19:55:39 -0500

    9.2.4 | js:2, simplified 2 consts into 1 const

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
 const generatePage = (userName, githubName) => {
     return `
