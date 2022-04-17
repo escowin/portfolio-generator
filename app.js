cc77ddccfd6c49cdedd86fcab72c25fc5dc7d165
escowin <edwin@escowinart.com>
2022-04-16 22:05:57 -0500

    9.3.5 | app.js:11-71 command line prompt sequence

--- app.js
+++ app.js
@@ -8,12 +8,64 @@ const inquirer = require('inquirer');
 //   if (err) throw err;
 // });
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: "what's your name?"
+            message: "enter your name"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'enter your github username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'enter info about yourself'
         }
-    ])
-    .then(answers => console.log(answers));
+    ]);
+};
+
+const promptProject = () => {
+    console.log(`
+    ===========
+    add project
+    ===========
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'enter project name'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'enter project description'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'what languages are used? check all that apply',
+            choices: ['javascript', 'html', 'css', 'es6', 'jQuery', 'bootstrap', 'node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'enter github project link (required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: "feature this project?",
+            default: false
+        },
+    ]);
+};
+
+promptUser()
+    .then(answers => console.log(answers))
+    .then(promptProject)
+    .then(projectAnswers => console.log(projectAnswers));
