c434e68bb8e04566d873ee4bc6edf3a6779e681b
escowin <edwin@escowinart.com>
2022-05-21 20:43:53 -0500

    9.2.4 | template literal

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const profileDataArgs = process.argv.slice(2);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+const generatepPage = (userName, githubName) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${userName}</h1>
+        <h2><a href="https://github.com/${githubName}">github</a></h2>
+    </body>
+    </html>
+    `;
+};
+
+console.log(name, github)
+console.log(generatepPage(name, github));
