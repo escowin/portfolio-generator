656458c0d0025f3f3110f4112141fa7d59ad9373
escowin <edwin@escowinart.com>
2022-05-21 23:31:56 -0500

    9.3.5 | succesful prompt recursion

--- app.js
+++ app.js
@@ -1,15 +1,95 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
 // inquirer.prompt([questions]).then(answers => {use user feedback});
-inquirer
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "your name:",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "your github:",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "about you:",
+    },
+  ]);
+};
+
+const promptProject = (portfolioData) => {
+  console.log(`
+    ===========
+    add project
+    ===========
+    `);
+
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  return inquirer
     .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'what is your name?'
-        }
+      {
+        type: "input",
+        name: "name",
+        message: "project name:",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "project description (required):",
+      },
+      {
+        type: "checkbox",
+        name: "languages",
+        message: "project languages (check all that apply):",
+        choices: [
+          "JavaScript",
+          "HTML",
+          "CSS",
+          "ES6",
+          "jQuery",
+          "Bootstrap",
+          "Node",
+        ],
+      },
+      {
+        type: "input",
+        name: "link",
+        message: "github repo (required):",
+      },
+      {
+        type: "confirm",
+        name: "feature",
+        message: "display as feature project?",
+        default: false,
+      },
+      {
+        type: "confirm",
+        name: "confirmAddProject",
+        message: "add another project?",
+        default: false,
+      },
     ])
-    .then(answers => console.log(answers));
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
@@ -21,4 +101,3 @@ inquirer
 
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
-
